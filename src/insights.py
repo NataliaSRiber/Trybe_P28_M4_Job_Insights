742a6b38269d41e90054534e2784ff94b40e2ac9
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-03-26 20:30:17 -0300

    requisito 6

--- src/insights.py
+++ src/insights.py
@@ -11,20 +11,12 @@ def get_unique_job_types(path):
 
 
 def filter_by_job_type(jobs, job_type):
-    """Filters a list of jobs by job_type
+    match_jobs = []
+    for job in jobs:
+        if job["job_type"] == job_type:
+            match_jobs.append(job)
 
-    Parameters ----------
-    jobs : list
-        List of jobs to be filtered
-    job_type : str
-        Job type for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided job_type
-    """
-    return []
+    return match_jobs
 
 
 def get_unique_industries(path):
