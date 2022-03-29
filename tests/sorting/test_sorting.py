8a838f5082dd8a0bf149162001c3c56cb7e82e1d
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2022-03-29 12:12:07 -0300

    requisito 10

--- tests/sorting/test_sorting.py
+++ tests/sorting/test_sorting.py
@@ -1,5 +1,37 @@
-# from src.sorting import sort_by
+from src.sorting import sort_by
+# min_salary_key
+
+
+jobs = [
+    {
+        "min_salary": 500,
+        "max_salary": 1500,
+        "date_posted": "2021-12-04",
+    },
+    {
+        "min_salary": 1500,
+        "max_salary": 2500,
+        "date_posted": "2021-11-09",
+    },
+    {
+        "min_salary": 800,
+        "max_salary": 2900,
+        "date_posted": "2021-09-23",
+    },
+    {
+        "min_salary": 700,
+        "max_salary": 1900,
+        "date_posted": "2021-01-23",
+    },
+]
 
 
 def test_sort_by_criteria():
-    pass
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["min_salary"] == 500
+
+    sort_by(jobs, "max_salary")
+    assert jobs[0]["max_salary"] == 2900
+
+    sort_by(jobs, "min_salary")
+    assert jobs[0]["date_posted"] == "2021-12-04"
